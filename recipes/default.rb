4e75d6e11196bbdb0bf63519e3734d86fd6f8fda
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-05-19 10:43:37 +0300

    initial implementation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,29 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+node['cog_mysql_backup']['packages'].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+template '/root/mysqlbackup.sh' do
+  action :create
+  mode '0755'
+  owner 'root'
+  group 'root'
+  source 'mysqlbackup.sh.erb'
+  variables({
+    :backupdest => node['cog_mysql_backup']['dest'],
+    :archivedir => node['cog_mysql_backup']['archive']
+    :mailto     => node['cog_mysql_backup']['email']
+    })
+end
+
+cron_d 'mysql-backup' do
+  minute  node['cog_mysql_backup']['minute']
+  hour    node['cog_mysql_backup'][hour]
+  command '/root/myslbackup.sh'
+  user    'root'
+end
