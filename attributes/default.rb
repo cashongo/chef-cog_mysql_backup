ef1bb427e53223b52ea09037622d48e632d05305
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-06-10 11:46:06 +0300

    add node.cog_mysql_backup.from_email attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['cog_mysql_backup']['dest'] = '/backup'
 default['cog_mysql_backup']['archive'] = '/archive'
 default['cog_mysql_backup']['email'] = 'root@localhost'
+default['cog_mysql_backup']['email_from'] = nil
 default['cog_mysql_backup']['packages'] = ['qpress','mailx','percona-xtrabackup']
 default['cog_mysql_backup']['hour'] = 1
 default['cog_mysql_backup']['minute'] = 0
