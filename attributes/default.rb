93b06d8f2c142015db329ff613b24d33cf2a479d
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2016-06-20 17:02:50 +0300

    define incremental backup url

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,7 @@ default['cog_mysql_backup']['hour'] = 1
 default['cog_mysql_backup']['minute'] = 0
 
 default['cog_mysql_backup']['s3url'] = 's3://cashongo-dev-mysql-archive/'
+default['cog_mysql_backup']['s3url_incr'] = 's3://cashongo-dev-mysql-backup/'
 
 default['cog_mysql_backup']['aws_credentials_vault']  = 'cog_mysql_backup'
 default['cog_mysql_backup']['aws_credentials_item']   = 'secrets'
